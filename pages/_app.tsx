f3919b26ae9654fdf609bfd5a1b0b5623506896f
Yonela Johannes <69614136+Yonela-Johannes@users.noreply.github.com>
2022-03-05 17:49:31 +0200

    Login session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 
 
-function MyApp({ Component, pageProps: {session, pageProps} }: AppProps) {
+function MyApp({
+   Component,
+    pageProps: {session, pageProps} }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
