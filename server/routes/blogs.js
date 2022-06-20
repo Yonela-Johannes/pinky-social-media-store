83588febae07ce32348507c3fe3dec7733ed21e0
Yonela-Johannes <johannesyonela@gmail.com>
2022-06-20 06:23:24 +0200

    removed directory

--- server/routes/blogs.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getBlogs, createBlog } from '../controllers/blogs.js';
-const router = express.Router();
-
-router.get('/', getBlogs)
-
-router.post('/', createBlog)
-
-export default router
